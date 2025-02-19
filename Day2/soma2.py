fab44b43e83e75c46139f09decad661a7d5fbccc
Onomad1 <henrike353535@gmail.com>
2025-02-19 16:26:20 -0300

    Adicionando duas novas formas de fazer soma

---
+++ Day2/soma2.py
@@ -0,0 +1,14 @@
+def soma(num1, num2):
+    resultado = num1 + num2
+    print(resultado)
+    return
+
+#soma(10, 13)
+def soma():
+    num1 = int(input("Digite o primeiro número"))
+    num2 = int(input("Digite o segundo número"))
+    resultado = num1 + num2
+    print(resultado)
+    return
+
+soma()
