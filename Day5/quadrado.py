3bc38d64d46dee39a42c391eecb328f5f20a76e8
Onomad1 <henrike353535@gmail.com>
2025-02-22 13:16:09 -0300

    adicionando dia 5

---
+++ Day5/quadrado.py
@@ -0,0 +1,8 @@
+def quadrado(num):
+    return num * num
+
+entrada = int(input("Escolha um número e eu direi o quadrado dele: "))
+
+resultado = quadrado(entrada)
+
+print(f"O quadrado de {entrada} é igual á {resultado}")
