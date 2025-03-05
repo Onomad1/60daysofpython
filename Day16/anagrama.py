ca0b2f9ac318c15b987cf98053b74a7c91d34be7
Onomad1 <henrike353535@gmail.com>
2025-03-05 14:38:53 -0300

    Descrição das mudanças

---
+++ Day16/anagrama.py
@@ -0,0 +1,9 @@
+def anagrama(palavra1, palavra2):
+    palavra1 = palavra1.replace(" ","").lower
+    palavra2 = palavra2.replace(" ","").lower
+    
+    return sorted
+
+palavra = input("Digite a sua primeira palavra:  ")
+
+print(sorted(palavra))
